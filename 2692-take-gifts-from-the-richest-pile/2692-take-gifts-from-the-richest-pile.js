81709ff71efded82a3cfe661d06310eac0560f35
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2024-12-12 21:27:37 +0200

    Time: 138 ms (5.09%), Space: 56 MB (7.23%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.js
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.js
@@ -5,11 +5,7 @@
  */
 var pickGifts = function(gifts, k) {
 
-    let total = 0;
-    for (let gift of gifts) {
-        total += gift;
-    }
-    
+  
     //we will solve it using max Heap 
  
  
@@ -17,13 +13,12 @@ var pickGifts = function(gifts, k) {
 for(let i = 0 ; i <k;i++){
     let largest = gifts[0];
     let new_value = Math.floor(Math.sqrt(largest));
-    total -= (largest-new_value)
     gifts[0] = Math.floor(Math.sqrt(largest))
     gifts.sort((a, b) => {return b - a; });
 }
 console.log(gifts)
 
- return total ;
+ return gifts.reduce((sum, gift) => sum + gift, 0); ;
 
     
 }; 
