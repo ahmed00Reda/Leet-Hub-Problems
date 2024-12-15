7defe2afe00fe94911256baeb139b423d9175404
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2024-12-15 15:59:13 +0200

    Time: 1 ms (29.18%), Space: 49.4 MB (32.21%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.js
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.js
@@ -6,10 +6,8 @@ var maximumWealth = function(accounts) {
     wealth = []
     for(i in accounts ){
         wealth[i]=accounts[i].reduce((accumulator, currentValue) => {
-            return accumulator + currentValue});
+            return accumulator + currentValue},0);
     }
     return Math.max(...wealth)
     
-//  another solution
-// return Math.max(...accounts.map(customer=>customer.reduce((a,b)=>a+b)));
 };
