9098fe392bf7d1cf978a0021383bc1646a6e919b
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2024-12-15 15:47:12 +0200

    Time: 0 ms (100%), Space: 50.7 MB (78.75%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
@@ -2,16 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-//    sumWithInitial = nums.reduce((accumulator, currentValue) =>
-//          accumulator + currentValue,0);
-
-var runningSum = function(nums) {
-    let newArr = []
-    sum=0
-    for(i in nums){
-        newArr[i]=sum+nums[i]
-        sum+=nums[i]
-    }
-    return newArr
-    
+const runningSum = nums => {
+  let sum = 0;
+  return nums.map(num => sum += num);
 };
