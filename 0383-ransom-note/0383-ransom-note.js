af578da0802a610780ed4fc6ac894fe67766f6d4
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2024-12-16 22:03:09 +0200

    Time: 10 ms (88.31%), Space: 55.7 MB (15.96%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -3,20 +3,32 @@
  * @param {string} magazine
  * @return {boolean}
  */
-var canConstruct = function(ransomNote, magazine) {
-    let Hash = {};
 
-    for (let c of magazine) {
-        Hash[c] = (Hash[c] || 0) + 1;
+var canConstruct = function(ransomNote, magazine) {
+    
+    // soleved using hashTable
+    // let Hash = {};
+    // for (let c of magazine) {
+    //     Hash[c] = (Hash[c] || 0) + 1;
+    // }
+    // for (let c of ransomNote) {
+    //     if (!Hash[c] || Hash[c] <= 0) {
+    //         return false;
+    //     }
+    //     Hash[c]--;
+    // }
+    
+    // return true;   
+    
+     if (ransomNote.length > magazine.length) {
+        return false;
     }
 
-    for (let c of ransomNote) {
-        if (!Hash[c] || Hash[c] <= 0) {
+    for (const c of new Set(ransomNote)) {
+        if (magazine.split(c).length - 1 < ransomNote.split(c).length - 1) {
             return false;
         }
-        Hash[c]--;
     }
-    
+
     return true;   
-    
 };
