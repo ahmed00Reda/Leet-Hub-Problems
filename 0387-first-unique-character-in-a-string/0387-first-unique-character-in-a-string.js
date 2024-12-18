be33cdba8b603e68f7ef719dbfb2c50ac232026c
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2024-12-18 23:21:18 +0200

    Time: 71 ms (6.65%), Space: 57.4 MB (14.89%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const hashTable = [];
+  
+  for (let char of s) {
+    hashTable[char] = (hashTable[char] || 0) + 1;
+  }
+  for(c in hashTable){
+    console.log(hashTable[c])
+    if(hashTable[c]==1){
+        return s.indexOf(c)
+    }
+   
+  }
+   return -1 
+
+    
+};
