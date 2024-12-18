c2c0fd68aab7b0108d3a5ebec5b37662f7968416
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2024-12-18 23:29:02 +0200

    Time: 31 ms (76.35%), Space: 52.4 MB (91.57%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,18 +3,27 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-      const map = {};
-    
-    for (const char of s) {
-        map[char] = (map[char] || 0) + 1;
-    }
-    
-    
-    for (let i = 0; i < s.length; ++i) {
-        if (map[s[i]] === 1) {
-            return i;
+
+     for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+            return i
         }
     }
-    return -1;
+    return -1
+
+    // another solution using hash map
+    // const map = {};
+    
+    // for (const char of s) {
+    //     map[char] = (map[char] || 0) + 1;
+    // }
+    
+    
+    // for (let i = 0; i < s.length; ++i) {
+    //     if (map[s[i]] === 1) {
+    //         return i;
+    //     }
+    // }
+    // return -1;
     
 };
