1304a557e76f14eb6f76b5a5df5f67f7b6b9a889
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2024-12-22 00:00:47 +0200

    Time: 1 ms (73.15%), Space: 49.3 MB (82.38%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.js
+++ 0682-baseball-game/0682-baseball-game.js
@@ -3,17 +3,25 @@
  * @return {number}
  */
 var calPoints = function(operations) {
-    const res = [];
-    const map = {
-        "+": () => res.push(res[res.length - 1] + res[res.length - 2]),
-        "D": () => res.push(res[res.length - 1] * 2),
-        "C": () => res.pop(),
+    const arr = []
+    for(let i=0;i<operations.length;i++){
+       let value = Number(operations[i])
+       if(!isNaN(value)){
+        arr.push(value)
+       }
+       else if(operations[i]==="C"){
+        arr.pop()
+       }
+       else if(operations[i]==="D"){
+         arr.push(arr[arr.length-1]*2)
+       }
+       else if(operations[i]==='+'){
+        arr.push(arr[arr.length-1]+arr[arr.length-2])
+       }
     }
+    const result = arr.reduce((acc,curr)=>
+        acc+=curr,0)
 
-    for (const op of operations) {
-        map?.[op] ? map[op]() : res.push(+op);
-    }
-
-    return res.reduce((acc, num) => acc + num, 0);
-};
+    return result
     
+};
