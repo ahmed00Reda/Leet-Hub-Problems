df0941a6aa75502d540dd16dccb176576134b9f7
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2024-12-25 00:46:54 +0200

    Time: 5 ms (80.22%), Space: 52.6 MB (48.13%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.js
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.js
@@ -4,24 +4,24 @@
  * @return {string}
  */
 var kthDistinct = function(arr, k) {
-  const hashMap = new Map();
-
-  for (const char of arr) {
-    hashMap.set(char, (hashMap.get(char) || 0) + 1);
-  }
-  let distinct={}
-  for([key,val] of hashMap){
-    if (val===1){
-        distinct[key]=val
+  let hash = new Map()
+    let i = 0
+    while (arr.length > i) {
+        if (hash.has(arr[i])) {
+            let before = hash.get(arr[i])
+            hash.set(arr[i], before + 1)
+        } else {
+            hash.set(arr[i], 1)
+        }
+        i++
     }
-  }
-  const entries = Object.entries(distinct);
-  if (k > 0 && k <= entries.length) {
-  const nthElement = entries[k - 1];
-  return nthElement[0] 
-} else {
-  return ""
-}
+    for (const [key, value] of hash) {
+        if (value == 1) {
+            k = k - 1
+            if (k <= 0) return key
+        }
+    }
+    return ""
 
 
 };
