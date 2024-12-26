c6f223e6c41c8c2fec747c543392ced834aa7684
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2024-12-26 22:18:28 +0200

    Time: 4 ms (93.97%), Space: 68.7 MB (89.22%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.js
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.js
@@ -5,15 +5,16 @@
  * @return {number[][]}
  */
 var construct2DArray = function(original, m, n) {
-    let k = 0;
-    let arr = [];
-    if (m * n != original.length) return [];
-    for (let i = 0; i < m; i++) {
-        arr[i] = [];
-        for (let j = 0; j < n; j++) {
-            arr[i][j] = original[k++];
+    let answer = []
+        if(original.length !== m*n){
+            return answer
         }
-    }
-    return arr;
+
+        for(let j = 0; j < n * m; j += n){
+         answer.push(original.slice(j,n + j))
+        } 
+     
+
+   return answer 
     
 };
