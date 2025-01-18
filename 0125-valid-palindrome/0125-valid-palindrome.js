3db15a0a6c80106c240b1b0703a1f350a392a80c
Ahmed Reda Abdelrahamn <62802199+ahmed00Reda@users.noreply.github.com>
2025-01-18 22:37:23 +0200

    Time: 4 ms (73.3%), Space: 53.7 MB (38.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,10 +4,7 @@
  */
 var isPalindrome = function(s) {
     const result = s.replace(/[^a-zA-Z0-9]+/g, "").toLowerCase();
-
     reverse = result.split('').reverse().join('').toLowerCase();
-    console.log(result)
-    console.log(reverse)
     return reverse ===result
     
 };
